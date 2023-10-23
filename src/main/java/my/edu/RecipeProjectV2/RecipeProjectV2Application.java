f199018a4dee243ec6942ddcb24ef47dd59aa599
claudrob <crobinson@tec.mx>
2023-10-23 12:40:22 -0600

    Initial project

---
+++ src/main/java/my/edu/RecipeProjectV2/RecipeProjectV2Application.java
@@ -0,0 +1,13 @@
+package my.edu.RecipeProjectV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/RecipeProjectV2/RecipeProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.RecipeProjectV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
